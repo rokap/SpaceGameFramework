3152d0b41010fff87a853a89ecc3061216aee08c
David Kaplan <dave@rokap.com>
2019-03-28 13:40:38 -0400

    INIT

---
+++ AI/Behaviours - Copy/Attacking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public partial class AI
+{
+    public partial class Behaviour
+    {
+        public class Attacking : Behaviour, IBehaviour
+        {
+            public Attacking(Entity owner) { this.owner = owner; }
+
+            public void Start()
+            {
+                Debug.Log("Starting " + this.GetType() + " Behaviour");
+            }
+
+            public void Update()
+            {
+                Debug.Log("Updating " + this.GetType() + " Behaviour");
+            }
+
+            public void End()
+            {
+                Debug.Log("Ending " + this.GetType() + " Behaviour");
+            }
+        }       
+    }
+}
---
+++ AI/Behaviours/Capturing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public partial class AI
+{
+    public partial class Behaviour
+    {
+        public class Capturing : Behaviour, IBehaviour
+        {
+            public Capturing(Entity owner) { this.owner = owner; }
+
+            public void Start()
+            {
+                Debug.Log("Starting " + this.GetType() + " Behaviour");
+            }
+
+            public void Update()
+            {
+                Debug.Log("Updating " + this.GetType() + " Behaviour");
+            }
+
+            public void End()
+            {
+                Debug.Log("Ending " + this.GetType() + " Behaviour");
+            }
+        }       
+    }
+}
